5ea0ea21b9bd71df01568d6ded0f0d226f51377f
amira <amirasaladin@hotmail.com>
2025-07-06 07:44:50 +0300

    Added queries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,35 @@
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.booking_date,
+    u.user_id,
+    u.name AS user_name,
+    u.email
+FROM 
+    bookings b
+INNER JOIN 
+    users u ON b.user_id = u.user_id;
+    
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+    reviews r ON p.property_id = r.property_id;
+
+-- Standard FULL OUTER JOIN (works in PostgreSQL, SQL Server, etc.)
+SELECT 
+    u.user_id,
+    u.name AS user_name,
+    b.booking_id,
+    b.property_id,
+    b.booking_date
+FROM 
+    users u
+FULL OUTER JOIN 
+    bookings b ON u.user_id = b.user_id;
